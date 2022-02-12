535228bb89b8bf1e6aec403bf6bb4b62e9ac8846
Fabián E. García <fabianegl79@gmail.com>
2022-02-12 16:47:15 -0300

    Mock de tareas e interfaces de las mismas

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,30 @@
+import {Task} from "./task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Pintar la casa",
+        day: 'Febrero 18 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Pintar la hamaca",
+        day: 'Febrero 25 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Cortar el pasto",
+        day: 'Febrero 26 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: "Rendir Final de Animación I",
+        day: 'Febrero 28 a las 08:00',
+        reminder: true,
+    },
+
+
+]
